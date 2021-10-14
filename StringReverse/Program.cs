67ef41bda73413a55787a9923e9cea51595498e1
vishal.Alok <you@example.com>
2021-10-15 02:59:04 +0530

    Add project files.

---
+++ StringReverse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            char[] C = s.ToCharArray();
+            string result = string.Empty;
+            for (int i = C.Length-1; i >= 0; i--)
+            {
+                result = result + Convert.ToString(C[i]);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
